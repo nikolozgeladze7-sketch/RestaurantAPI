bce1e995d281922e0847bbb1073d7847466d7813
admin <admin@DESKTOP-TC01TCH>
2026-03-14 21:39:42 +0400

    Add project files.

---
+++ RestaurantAPI/Controllers/ProductController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Data;
+using RestaurantAPI.Model.Entities;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(RestaurantDbContext context) : ControllerBase
+    {
+        private readonly RestaurantDbContext context = context;
+
+        [HttpGet("GetFood")]
+        public async Task<IActionResult> GetFood()
+        {
+            var food = await context.Product.ToListAsync();
+            return Ok(food);
+        }
+
+        [HttpGet("GetFoodById/{id}")]
+        public async Task<IActionResult> GetFoodById(int id)
+        {
+            var food = await context.Product.FirstOrDefaultAsync(x => x.Id == id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+            return Ok(food);
+        }
+
+        [HttpPost("AddFood")]
+        public async Task<IActionResult> AddFood(Model.Entities.Product newProduct)
+        {
+            Product product = new Product();
+            product.Id = newProduct.Id;
+            product.Name = newProduct.Name;
+            product.Price = newProduct.Price;
+            product.Nuts = newProduct.Nuts;
+            product.Image = newProduct.Image;
+            product.Vegetarian = newProduct.Vegetarian;
+            product.Spiciness = newProduct.Spiciness;
+            product.CategoryId = newProduct.CategoryId;
+
+            await context.Product.AddAsync(product);
+            await context.SaveChangesAsync();
+            return Ok(product);
+        }
+
+        [HttpPut("UpdateFood/{id}")]
+        public async Task<IActionResult> UpdateFood(int id, Model.Entities.Product product)
+        {
+            if (id != product.Id) 
+            {
+                return BadRequest();
+            } 
+            context.Product.Update(product);
+            await context.SaveChangesAsync();
+            return Ok(product);
+        }
+
+        [HttpDelete("DeleteFood/{id}")]
+        public async Task<IActionResult> DeleteFood(int id)
+        {
+            var food = await context.Product.FirstOrDefaultAsync(x => x.Id == id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+            context.Product.Remove(food);
+            await context.SaveChangesAsync();
+            return Ok(food);
+        }
+    }
+}
---
+++ RestaurantAPI/Data/RestaurantDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Model.Entities;
+
+namespace RestaurantAPI.Data
+{
+    public class RestaurantDbContext : IdentityDbContext
+    {
+        public RestaurantDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected RestaurantDbContext()
+        {
+        }
+
+        public DbSet<Product> Product {  get; set; }
+    }
+}
