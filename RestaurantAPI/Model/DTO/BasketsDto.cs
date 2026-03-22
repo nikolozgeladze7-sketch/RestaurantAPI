ac0aa229056277128e0588738c5d0b4e1c6d2663
NikolozGeladze <nikolozgeladze7@gmail.com>
2026-03-22 11:39:42 +0400

    Add files via upload

---
+++ RestaurantAPI/Model/DTO/BasketsDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantAPI.Model.DTO
+{
+    public class BasketsDto
+    {
+        [Key]
+        [Column("dishId")]
+        public int DishId { get; set; }
+        [Column("price")]
+        public int Price { get; set; }
+        [Column("quantity")]
+        public int Quantity { get; set; }
+    }
+}
