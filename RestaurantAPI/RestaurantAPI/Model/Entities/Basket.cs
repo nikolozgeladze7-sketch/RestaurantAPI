4882e0227df1a1234b0e5bada590cfe9ffc9895f
NikolozGeladze <nikolozgeladze7@gmail.com>
2026-04-01 17:52:58 +0400

    Add files via upload

---
+++ RestaurantAPI/RestaurantAPI/Model/Entities/Basket.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using RestaurantAPI.Model.DTO;
+
+namespace RestaurantAPI.Model.Entities
+{
+    public class Basket
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("dishId")]
+        public int DishId { get; set; }
+
+        [Column("price")]
+        public int Price { get; set; }
+
+        [Column("quantity")]
+        public int Quantity { get; set; }
+    }
+}
---
+++ RestaurantAPI/RestaurantAPI/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Data;
+using RestaurantAPI.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string connectionString = builder.Configuration.GetConnectionString("RestaurantDataBase") ?? string.Empty;
+builder.Services.AddDbContext<RestaurantDbContext>(options => options.UseSqlServer(connectionString));
+
+// identity
+
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IDishService, DishService>();
+builder.Services.AddScoped<IBasketService, BasketService>();
+
+//Add Cors
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//Use Cors
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync();
